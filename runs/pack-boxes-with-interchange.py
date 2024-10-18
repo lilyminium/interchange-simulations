f30d96c356012d242187b986545d0bc71f41fb82
Lily Wang <lily@mdanalysis.org>
2024-10-18 19:10:20 +1100

    add initial files

---
+++ runs/pack-boxes-with-interchange.py
@@ -0,0 +1,65 @@
+import json
+import pathlib
+import click
+import tqdm
+
+from openff.units import unit
+from openff.units.openmm import from_openmm
+from openmm import NonbondedForce
+from openff.toolkit import Molecule, Topology, ForceField
+from openff.interchange.components._packmol import pack_box, UNIT_CUBE
+from openff.interchange import Interchange
+
+TARGET_DENSITY = 0.95 * unit.grams / unit.mL
+
+def main(
+    input_file: str,
+    force_field: str,
+    output_directory: str,
+):
+    with open(input_file, "r") as f:
+        data = json.load(f)
+
+    force_field = ForceField(force_field)
+    output_directory = pathlib.Path(output_directory)
+
+    for i, box in enumerate(tqdm.tqdm(data, desc="packing boxes")):
+        mols = [
+            Molecule.from_smiles(smiles, allow_undefined_stereo=True) for smiles in box["smiles"]
+        ]
+        for mol in mols:
+            mol.generate_conformers()
+        n_molecules = box["n_molecules"]
+
+        solute = None
+        if n_molecules[0] == 1:
+            solute = mols.pop(0).to_topology()
+            n_molecules.pop(0)
+        
+        solvated_topology = pack_box(
+            molecules=mols,
+            number_of_copies=n_molecules,
+            solute=solute,
+            target_density=TARGET_DENSITY,
+            box_shape=UNIT_CUBE,
+            center_solute=True,
+            working_directory=None,
+            retain_working_files=False,
+        )
+
+        interchange = Interchange.from_smirnoff(force_field, solvated_topology)
+
+        entry_directory = output_directory / f"entry-{i:04d}"
+        entry_directory.mkdir(parents=True, exist_ok=True)
+
+        serialized_file = entry_directory / "interchange.json"
+        with open(serialized_file, "w") as f:
+            interchange.json(serialized_file, format=True)
+        
+        interchange.to_pdb(entry_directory / "input.pdb")
+        interchange.to_gro(entry_directory / "input.gro")
+        interchange.to_top(entry_directory / "system.top")
+
+
+if __name__ == "__main__":
+    main()
