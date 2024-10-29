75eba400be29f8276e068eaa755193062bb317ba
Lily Wang <lily@mdanalysis.org>
2024-10-29 16:17:13 +1100

    add evaluator runs

---
+++ runs/run-pack-boxes-with-evaluator.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#SBATCH -J pack-boxes
+#SBATCH --array=0-1411%100
+#SBATCH -p free
+#SBATCH -t 24:00:00
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16gb
+#SBATCH --account dmobley_lab
+#SBATCH --output run-logs/slurm-%x.%A-%a.out
+
+# ===================== conda environment =====================
+. ~/.bashrc
+conda activate evaluator-0-4-10
+
+# 1000: 1412 boxes
+
+NMOL=1000
+
+if [ ! -f "input.pdb" ] ; then
+    python pack-boxes-with-interchange.py  -i "n-${NMOL}/liquid-boxes.json" -o "n-${NMOL}/runs-evaluator" -idx $SLURM_ARRAY_TASK_ID
+fi
+
+
